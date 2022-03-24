980538393e00d958e659a694997f89ad4655269a
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-03-24 15:06:44 -0700

    implemented login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaSignInAlt } from 'react-icons/fa';
 import { toast } from 'react-toastify';
-
 import { useSelector, useDispatch } from 'react-redux';
 
-import { login } from '../features/auth/authSlice';
+import { login, reset } from '../features/auth/authSlice';
 
 const Login = () => {
     const [formData, setFormData] = useState({
@@ -14,9 +14,29 @@ const Login = () => {
 
     const { email, password } = formData;
 
+    const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    const { user, isSuccess, isLoading, message } = useSelector(state => state.auth);
+    const {
+        user,
+        isError,
+        isSuccess,
+        isLoading,
+        message
+    } = useSelector(state => state.auth);
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        // Redirect when logged in successfully
+        if (isSuccess || user) {
+            navigate('/');
+        }
+
+        dispatch(reset());
+    }, [isError, isSuccess, user, message, navigate, dispatch]);
 
     const onChange = (e) => {
         setFormData((prevState) => ({
