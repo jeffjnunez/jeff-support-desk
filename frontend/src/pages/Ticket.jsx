8142226c322b1d8a789163d1624b86966e8e9da0
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-03-26 19:51:47 -0700

    display notes in frontend

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 
 import { getTicket, closeTicket, reset } from '../features/tickets/ticketSlice';
+import { getNotes, reset as notesReset } from '../features/notes/noteSlice';
+import NoteItem from '../components/NoteItem';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 
@@ -16,6 +18,11 @@ const Ticket = () => {
         message
     } = useSelector((state) => state.tickets);
 
+    const {
+        notes,
+        isLoading: notesIsLoading,
+    } = useSelector((state) => state.notes);
+
     const params = useParams();
     const navigate = useNavigate();
     const dispatch = useDispatch();
@@ -27,6 +34,7 @@ const Ticket = () => {
         }
 
         dispatch(getTicket(ticketId));
+        dispatch(getNotes(ticketId));
     }, [isError, message, ticketId]);
 
     const onTicketClose = () => {
@@ -35,7 +43,7 @@ const Ticket = () => {
         navigate('/tickets');
     };
 
-    if (isLoading) {
+    if (isLoading || notesIsLoading) {
         return <Spinner />;
     }
 
@@ -60,7 +68,16 @@ const Ticket = () => {
                     <h3>Description of Issue</h3>
                     <p>{ticket.description}</p>
                 </div>
+                <h2>Notes</h2>
             </header>
+
+            {notes.length === 0 ? (
+                <span>No notes yet.</span>
+            ) : (
+                notes.map((note) => (
+                    <NoteItem key={note._id} note={note} />
+                ))
+            )}
 
             {ticket.status !== 'closed' && (
                 <button onClick={onTicketClose} className='btn btn-block btn-danger'>Close Ticket</button>
